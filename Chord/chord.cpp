b59cce72851616f137be87b358fc98e74e2c6de1
MacBook Pro <macbookpro@wireless-10-104-83-97.umd.edu>
2021-11-19 08:17:34 -0500

    basic structure chord

--- Chord/chord.cpp
+++ Chord/chord.cpp
@@ -13,11 +13,13 @@
 #include <fstream>
 #include <math.h>
 #include <unistd.h>
+#include <string>
 // #include "node.h" // header in local directory
 
 // using namespace N;
 using std::cout;
 using std::endl;
+using std::string;
 
 int check = 0;
 struct chord_arguments
@@ -29,7 +31,7 @@ struct chord_arguments
   int tff; //time between invocations of 'fix fingers'
   int tcp; //time between invocations of ‘check predecessor'
   int r_succ; //number of successors maintained by the Chord client
-  int id_opt;
+  string id_opt;
 
 };
 
@@ -45,18 +47,19 @@ class Local_Node {
     int t_fix_fing; //time between invocations of 'fix fingers'
     int t_chck_pred; //time between invocations of ‘check predecessor'
     int r_succ; //number of successors maintained by the Chord client
-    int id_opt;
+    string id_opt;
     int sock_fd;
     // string node_id;
     
-    Local_Node(struct sockaddr_in m_port, struct sockaddr_in p_port, int ts, int tff, int t_cp, int r_s, int id_o){
+    Local_Node(struct sockaddr_in m_port, struct sockaddr_in p_port, int ts, int tff, int t_cp, int r_s, string id_o){
       my_iport.sin_port = htons(m_port.sin_port);
       my_iport.sin_addr.s_addr = INADDR_ANY;
       my_iport.sin_family = AF_INET;
 
       peer_iport.sin_port = htons(p_port.sin_port);
       peer_iport.sin_family = AF_INET;
-
+      predecessor = this;
+      successor = this;
       t_stb = ts;
       t_fix_fing = tff;
       t_chck_pred = t_cp;
@@ -71,6 +74,7 @@ class Local_Node {
         exit(EXIT_FAILURE);
       }else{
 		    printf("Socket created\n");
+        node->sock_fd = sock_fd;
 	    }
 
       if (bind(sock_fd, (const struct sockaddr *)&my_iport, sizeof(my_iport)) < 0 ){
@@ -88,6 +92,9 @@ class Local_Node {
       }
     }
 };
+
+Local_Node *node;
+
 uint8_t *hash(const char *lookup) {
 	uint8_t checksum[40];
 	uint8_t *ret;
@@ -116,6 +123,16 @@ uint8_t *hash(const char *lookup) {
 	return ret;
 }
 
+uint8_t *create_id(sockaddr_in ipp){
+
+  struct in_addr ip = ipp.sin_addr;
+  string ip_s = inet_ntoa(ip);
+  string str = ip_s + std::to_string(ipp.sin_port);
+
+  const char * c = str.c_str();
+  uint8_t *id = hash(c);
+  return id;
+};
 
 
 error_t chord_parser(int key, char *arg, struct argp_state *state)
@@ -227,58 +244,91 @@ chord_arguments chord_parseopt(int argc, char *argv[])
     return args;
 }
 
+void create (Local_Node *node){
+  //check for ID
+  if(node->id_opt.empty()){
+    memcpy(&node->id_opt,create_id(node->my_iport),40);
+  }
+
+  //Successor points to self
+  node->successor = node;
+
+  //Predecessor point to null
+  node->predecessor = NULL;
+}
+
+void notify(Local_Node *succ){
+// if (predecessor of succ is nil or n′ ∈ (predecessor, n)) predecessor = n′;
+if(succ->predecessor == NULL || node != succ->predecessor){
+  succ->predecessor = node;
+}
+}
+
+
+// successor, and tells the successor about n. 
+void stabilize(){
+Local_Node *temp = node->successor->predecessor;
+if(temp != node)
+{
+  node->successor = temp;
+}
+notify(node->successor);
+}
+
+
+Local_Node *find_successor(sockaddr_in ipp, string id){
+
+  //connect
+  //ask for successors
+  //recieve successors
+
+  //close socket
+  //return successor
+}
+
+void join (Local_Node *node){
+  //findsuccessor in the new ring
+  node->successor = find_successor(node->peer_iport, node->id_opt);
+}
+
+
+//if recieved process command
+void processCommand(int clientSock, uint8_t command)
+{
+	 switch (command) {
+		//join room
+		case 'Lookup': 
+			break;
+
+	default:
+		ARGP_ERR_UNKNOWN;
+		break;
+	}
+
+}
+
 
 int main(int argc, char *argv[])
 {
-
-    // chord_arguments client = client_parseopt(argc, argv);
-    // int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    // struct sockaddr_in serv_addr = client.ip_port;
-    //   int port = 4000;
-    //   struct sockaddr_in servaddr;
-    //   servaddr.sin_family = AF_INET;
-    //   servaddr.sin_addr.s_addr = INADDR_ANY;
-    //   servaddr.sin_port = htons(port);
-
-    // GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-
     chord_arguments chord = chord_parseopt(argc, argv);
     printf("Got %s port %d with ts=%d tff=%d tcp=%d r=%d \n", inet_ntoa(chord.my_iport.sin_addr), chord.my_iport.sin_port, chord.ts, chord.tff, chord.tcp, chord.r_succ);
     printf("Got %s port %d for peer \n", inet_ntoa(chord.peer_iport.sin_addr), chord.peer_iport.sin_port);
 
-    Local_Node node(chord.my_iport, chord.peer_iport, chord.ts, chord.tff, chord.tcp, chord.r_succ, chord.id_opt);
+    Local_Node new_node(chord.my_iport, chord.peer_iport, chord.ts, chord.tff, chord.tcp, chord.r_succ, chord.id_opt);
+    node = &new_node;
+
     if (check == 0)
     {
-        node.node_socket_initialize();
-        //! Nistha to add hashing here
-        //! 
-
-        // node.
-
+        node->node_socket_initialize();
+    
         //Create a new ring
+        create (node);
     }
     else if (check == 2)
     {
-        node.node_socket_initialize();
+        node->node_socket_initialize();
         printf("join ring \n");
         //join existing ring
     }
 
-    // string input;
-
-    // while(1){
-    //     cout << "send or recv: ";
-    //     getline(cin, input);
-
-    //     if(input.compare("send") == 0){
-    //         string myString = "SAARA ZAMAANA HASEENO KA DIWAANA";
-    //         std::vector<uint8_t> myVector(myString.begin(), myString.end());
-    //         uint8_t *p = &myVector[0];
-    //         node.send_call(p);
-    //     }else if (input.compare("recv") == 0){
-    //         node.recv_call();
-    //     }
-        
-    // }
 }
